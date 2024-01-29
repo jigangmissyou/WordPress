4b3a9b29821b754ae9f51903eafd8d269af8ceb4
Jigang <707394840@qq.com>
2024-01-29 14:13:54 +1300

    test

--- wp-content/plugins/tradehouse/controllers/PropertyController.php
+++ wp-content/plugins/tradehouse/controllers/PropertyController.php
@@ -12,25 +12,74 @@ class PropertyController {
         // add_action('wp_enqueue_scripts', array($this->view, 'enqueue_styles'));
         add_action('init', array($this->model, 'register_post_type'));
         add_action('add_meta_boxes', array($this->model, 'custom_fields'));
-        add_action('save_post', array($this->model, 'save_custom_fields'));
-        add_action('rest_api_init', array($this, 'tradehouse_watchlist'));
+        add_action('save_post', array($this->model, 'save_custom_fields'), 10, 2); // Increase priority and pass 2 arguments
+        add_action('rest_api_init', array($this, 'registerRestRoutes'));
+        add_filter( 'jwt_auth_whitelist', function ( $endpoints ) {
+            $tradehouse_endpoints = array(
+                '/wp-json/tradehouse/v1/get-renting-house',
+                '/wp-json/tradehouse/v1/categories/',
+            );
+            return array_unique( array_merge( $endpoints, $tradehouse_endpoints ) );
+        } );
+    }
+    
+    public function registerRestRoutes() {
+        // Watchlist routes
+        $this->tradehouse_watchlist();
+        // News routes
+        register_rest_route('tradehouse/v1', '/get-news/', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'getNews'),
+            'permission_callback' => '__return_true', 
+
+        ));
+
+        // Renting house routes
+        register_rest_route('tradehouse/v1', '/get-renting-house/', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'getRentingHouse'),
+            'permission_callback' => '__return_true',
+
+        ));
+
+        register_rest_route('tradehouse/v1', '/get-picture/', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'getPicturesFromCategory'),
+            'permission_callback' => '__return_true', // Add this line
+
+        ));
+
+        register_rest_route('tradehouse/v1', '/categories/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'get_category_hierarchy'),
+            'permission_callback' => '__return_true', // Add this line
+
+        ));
+
+       
     }
 
     public function tradehouse_watchlist() {
         // Add to watchlist
         register_rest_route('tradehouse/v1', '/add-to-watchlist/', array(
             'methods' => 'POST',
-            'callback' => array($this, 'handleWatchlistSubmission')
+            'callback' => array($this, 'handleWatchlistSubmission'),
+            'permission_callback' => '__return_true', // Add this line
+
         ));
         // Get user watchlist
         register_rest_route('tradehouse/v1', '/get-user-watchlist/', array(
             'methods' => 'GET',
             'callback' => array($this, 'getUserWatchlist'),
+            'permission_callback' => '__return_true', // Add this line
+
         ));
         // Cancle watchlist
         register_rest_route('tradehouse/v1', '/cancle-watchlist/', array(
             'methods' => 'POST',
             'callback' => array($this, 'cancelWatchlist'),
+            'permission_callback' => '__return_true', // Add this line
+
         ));
     }
 
@@ -67,6 +116,42 @@ class PropertyController {
         $this->model->removeFromUserWatchlist($user_id, $property_id);
 
         return $this->view->renderSuccessResponse('Property removed from watchlist.');
+    }
+
+    public function getNews($data) {
+        // Implement logic to retrieve news from the model
+        return $this->model->getNews($data['category_id']);
+    }
+
+    public function getPicturesFromCategory($data) {
+        // Implement logic to retrieve featured picture from the model
+        return $this->model->getPicturesFromCategory($data['category_slug']);
+    }
+
+    public function getRentingHouse() {
+        // Implement logic to retrieve featured picture from the model
+        return $this->model->getHousingRentPosts();
+    }
+
+    function get_category_hierarchy($data) {
+        $parent_id = isset($data['id']) ? intval($data['id']) : 0;
+    
+        $categories = get_categories(array(
+            'parent' => $parent_id,
+            'hide_empty' => false, // Include categories with no posts
+        ));
+    
+        $result = array();
+    
+        foreach ($categories as $category) {
+            $result[] = array(
+                'id' => $category->term_id,
+                'name' => $category->name,
+                'children' => $this->get_category_hierarchy(array('id' => $category->term_id)), // Recursive call
+            );
+        }
+    
+        return $result;
     }
 }
 ?>
--- wp-content/plugins/tradehouse/models/PropertyModel.php
+++ wp-content/plugins/tradehouse/models/PropertyModel.php
@@ -64,20 +64,44 @@ class PropertyModel {
         register_taxonomy('housing_rent_category', 'housing_rent', $taxonomy_args);
     
         register_post_type('housing_rent', $args);
+        register_rest_field('housing_rent', 'custom_fields', array(
+            'get_callback' => array($this, 'get_custom_fields'),
+            'update_callback' => null,
+            'schema' => null,
+        ));
+        // $this->custom_fields();
+    }
+
+    function get_custom_fields($object, $field_name, $request) {
+        $post_id = $object['id'];
+    
+        $custom_fields = array(
+            'rent_price'         => get_post_meta($post_id, '_rent_price', true),
+            'payment_frequency'  => get_post_meta($post_id, '_payment_frequency', true),
+            'bedrooms'           => get_post_meta($post_id, '_bedrooms', true),
+            'bathrooms'          => get_post_meta($post_id, '_bathrooms', true),
+            'parking_spaces'     => get_post_meta($post_id, '_parking_spaces', true),
+            'parking_type'       => get_post_meta($post_id, '_parking_type', true),
+            'availability_date'  => get_post_meta($post_id, '_availability_date', true),
+            'pet_allowed'        => get_post_meta($post_id, '_pet_allowed', true),
+            'smoker_allowed'     => get_post_meta($post_id, '_smoker_allowed', true),
+        );
+    
+        return $custom_fields;
     }
 
     function custom_fields() {
         add_meta_box(
             'tradehouse_custom_fields',
             'Housing Rent Details',
-            'tradehouse_render_custom_fields',
+            array($this, 'tradehouse_render_custom_fields'),
             'housing_rent',
             'normal',
             'default'
         );
     }
 
-    function render_custom_fields($post) {
+    function tradehouse_render_custom_fields($post) {
         $rent_price = get_post_meta($post->ID, '_rent_price', true);
         $payment_frequency = get_post_meta($post->ID, '_payment_frequency', true);
         $bedrooms = get_post_meta($post->ID, '_bedrooms', true);
@@ -124,9 +148,10 @@ class PropertyModel {
         <?php
     }
 
-    function save_custom_fields($post_id, $post) {
+    function save_custom_fields($post_id, $post) {    
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-    
+        if (!current_user_can('edit_post', $post_id)) return;
+        if ($post->post_type != 'housing_rent') return;
         $fields = array(
             'rent_price',
             'payment_frequency',
@@ -140,10 +165,8 @@ class PropertyModel {
         );
     
         foreach ($fields as $field) {
-            if (isset($post[$field])) {
-                update_post_meta($post_id, '_' . $field, sanitize_text_field($post[$field]));
-            } else {
-                delete_post_meta($post_id, '_' . $field);
+            if (isset($_POST[$field])) {
+                update_post_meta($post_id, '_' . $field, sanitize_text_field($_POST[$field]));
             }
         }
     }
@@ -177,5 +200,139 @@ class PropertyModel {
         $watchlist = array_diff($watchlist, array($property_id));
         update_user_meta($user_id, 'watchlist', $watchlist);
     }
+
+    public function getNewsByCategory($category_id) {
+        // Implement your logic to retrieve news data for a specific category
+        $args = array(
+            'post_type'      => 'news',
+            'posts_per_page' => -1, // Retrieve all news posts
+            'orderby'        => 'date',
+            'order'          => 'DESC',
+            'tax_query'      => array(
+                array(
+                    'taxonomy' => 'category', // Change this to your actual taxonomy name
+                    'field'    => 'id',
+                    'terms'    => $category_id,
+                ),
+            ),
+        );
+
+        $news_query = new WP_Query($args);
+
+        $news = array();
+
+        if ($news_query->have_posts()) {
+            while ($news_query->have_posts()) {
+                $news_query->the_post();
+
+                // Customize this part based on your actual data structure
+                $news[] = array(
+                    'title'   => get_the_title(),
+                    'content' => get_the_content(),
+                    'date'    => get_the_date(),
+                    // Add more fields as needed
+                );
+            }
+
+            wp_reset_postdata();
+        }
+
+        return $news;
+    }
+
+    public function getPicturesFromCategory($category_slug) {
+        $args = array(
+            'post_type'      => 'attachment',
+            'post_status'    => 'inherit',
+            'media_category' => $category_slug, 
+        );
+    
+        $attachments = get_posts($args);
+    
+        $picture_urls = array();
+        foreach ($attachments as $attachment) {
+            $picture_urls[] = wp_get_attachment_url($attachment->ID);
+        }
+    
+        return $picture_urls;
+    }
+
+    public function getHousingRentPosts($data = []) {
+        $args = array(
+            'post_type'      => 'housing_rent',
+            'posts_per_page' => -1,
+            // 'meta_query'     => array(
+            //     'relation' => 'AND',
+            //     // array(
+                //     'key'     => '_bedrooms',
+                //     'value'   => array($minBedrooms, $maxBedrooms),
+                //     'type'    => 'NUMERIC',
+                //     'compare' => 'BETWEEN',
+                // ),
+                // array(
+                //     'key'     => '_bathrooms',
+                //     'value'   => array($minBathrooms, $maxBathrooms),
+                //     'type'    => 'NUMERIC',
+                //     'compare' => 'BETWEEN',
+                // ),
+                // 可以添加更多的条件...
+            // ),
+            // 'tax_query'      => array(
+            //     array(
+            //         'taxonomy' => 'region_taxonomy', // 替换为你的地理区域的自定义分类法
+            //         'field'    => 'slug',
+            //         'terms'    => $region,
+            //     ),
+            // ),
+            'orderby'        => 'meta_value_num',
+            'order'          => 'ASC',
+        );
+
+        // 添加价格条件
+        if (isset($_GET['min_price']) && isset($_GET['max_price'])) {
+            $minPrice = (int) $_GET['min_price'];
+            $maxPrice = (int) $_GET['max_price'];
+            $args['meta_query'][] = array(
+                'key'     => '_rent_price',
+                'value'   => array($minPrice, $maxPrice),
+                'type'    => 'NUMERIC',
+                'compare' => 'BETWEEN',
+            );
+        }
+
+    
+        $query = new WP_Query($args);
+    
+        $posts = array();
+    
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $post_data = array(
+                    'id'    => get_the_ID(),
+                    'title' => get_the_title(),
+                    'link'  => get_the_permalink(),
+                    'houseImg' => get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'),
+                    'price' => get_post_meta(get_the_ID(), '_rent_price', true),
+                    'bedrooms' => get_post_meta(get_the_ID(), '_bedrooms', true),
+                    'parkings' => get_post_meta(get_the_ID(), '_parking_spaces', true),
+                    'parking_type' => get_post_meta(get_the_ID(), '_parking_type', true),
+                    'availability_date' => get_post_meta(get_the_ID(), '_availability_date', true),
+                    'pet_allowed' => get_post_meta(get_the_ID(), '_pet_allowed', true),
+                    'smoker_allowed' => get_post_meta(get_the_ID(), '_smoker_allowed', true),
+                    'created_at' => get_the_date(),
+                    
+                    // 添加其他字段...
+                );
+    
+                $posts[] = $post_data;
+            }
+            wp_reset_postdata();
+        }
+    
+        return rest_ensure_response($posts);
+    }
+    
+    
 }
 ?>
